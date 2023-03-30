de3ec014c71010232a6741640b2c22ad17af1520
Mauro Crispim <mcrispim@hotmail.com>
2023-03-29 22:06:17 -0300

    Cria modelos: MuseumObject e Department

---
+++ app/src/main/java/com/example/mytestapp/model/MuseumObject.kt
@@ -0,0 +1,12 @@
+package com.example.mytestapp.model
+
+data class MuseumObject(
+    val objectID: Int,
+    val primaryImage: String,
+    val primaryImageSmall: String,
+    val department: String,
+    val objectName: String,
+    val objectTitle: String,
+    val artistName: String,
+    val objectDate: String
+)
