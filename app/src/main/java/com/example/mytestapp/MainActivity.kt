9e22df84e29f33f76a835496bd82cd1184abd58c
Mauro Crispim <mcrispim@hotmail.com>
2023-03-29 23:48:58 -0300

    Instancia MetService na MainActivity

--- app/src/main/java/com/example/mytestapp/MainActivity.kt
+++ app/src/main/java/com/example/mytestapp/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.mytestapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mytestapp.databinding.ActivityMainBinding
+import com.example.mytestapp.network.MetService
+import com.example.mytestapp.network.RetrofitInitializer
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var service: MetService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        service = RetrofitInitializer().metService()
+
     }
 }
